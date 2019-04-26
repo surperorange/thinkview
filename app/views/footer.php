476eb4e5e575927f8066c8715c444539bab75c4e
surperorange <mail@gongcishi.com>
2019-04-26 12:03:51 +0800

    页头页尾调换。

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,7 +1,2 @@
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title><?php echo $title ?></title>
-</head>
-<body>
-<h1><?php echo $title ?></h1>
+</body>
+</html>
--- app/views/header.php
+++ app/views/header.php
@@ -1,2 +1,7 @@
-</body>
-</html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title><?php echo $title ?></title>
+</head>
+<body>
+<h1><?php echo $title ?></h1>
